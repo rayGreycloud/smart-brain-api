64fba85b34ac7cd967c5c552ef75fca14dd50ca7
rayGreycloud <rayGreycloud@gmail.com>
2019-02-06 13:17:43 -0800

    update db options

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
+    user : 'postgres',
+    password : 'test',
     database : 'smart-brain'
   }
 });
