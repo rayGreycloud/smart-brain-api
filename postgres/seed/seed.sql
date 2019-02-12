c299bf31f371ebb4b26691e6af0a3b4fa611eb77
rayGreycloud <rayGreycloud@gmail.com>
2019-02-12 08:44:33 -0800

    Update seed data to include age and pet

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT into users (name, email, entries, joined) values ('Obi-wan', 'obi-wan@jedi.com', 5, '2019-02-09');
+INSERT into users (name, email, entries, joined, age, pet) values ('Obi-wan', 'obi-wan@jedi.com', 5, '2018-11-30', 56, 'R2D2' );
 
 INSERT into login (hash, email) values ('$2a$10$B95egkRp0JJ9fbpeLY6Sd.wHcp.9u3H55njOekbcxjeLnowAq5Esa', 'obi-wan@jedi.com');
 
